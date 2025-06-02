413ffff62e733be5ebc40fe198ab02ce65cab26d
krishankant <www.krishankanttyagi201206@gmail.com>
2025-06-02 19:35:11 +0530

    category functionality added

--- Project-1/src/components/Home.jsx
+++ Project-1/src/components/Home.jsx
@@ -1,20 +1,47 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import Nav from "./Nav"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { ProductContext } from "../utils/Context"
 import Loading from "./Loading";
+import axios from '../utils/axios';
+
 
 export default function Home() {
 
   const [products] = useContext(ProductContext);
-  console.log(products);
+
+  const { search } = useLocation();
+  const category = decodeURIComponent(search.split("=")[1]);
+
+  
+  const [filteredProducts, setfilteredProducts] = useState(null)
+  
+  const getproductscategory = async () => {
+    try {
+      const { data } = await axios.get(`/products/category/${category}`);
+      
+      setfilteredProducts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  useEffect(() => {
+    if (!filteredProducts || category=='undefined') setfilteredProducts(products);
+    if (category != "undefined") getproductscategory();
+    }
+  , [category, products]);
+  
+  
+  
+
   return products ? (
     <>
       <Nav />
-      <div className=" bg-zinc-100 w-[85%] p-5 flex flex-wrap gap-6 overflow-x-hidden">
+      <div className=" bg-zinc-100 w-[85%] p-5 pt-8 flex flex-wrap gap-6 overflow-x-hidden">
 
-        {products.map((p, i) =>
-        <Link key={p.id} to={`/details/${p.id}`} className="card  p-2 mt-3  shadow rounded w-[16%]   ">
+        {filteredProducts && filteredProducts.map((p, i) =>
+        <Link key={p.id} to={`/details/${p.id}`} className="card  p-2 mt-8  shadow rounded w-[16%]   ">
         <div className="w-[90%] h-[26vh] card m-1 hover:scale-110 shadow-md rounded-md overflow-hidden ">
           <img
             className="w-full  h-42"
--- Project-1/src/components/Nav.jsx
+++ Project-1/src/components/Nav.jsx
@@ -1,6 +1,16 @@
-
+import React from "react";
+import { useContext } from "react";
+import { ProductContext } from "../utils/Context";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
+  const [products] = useContext(ProductContext);
+
+  let distinct_category = products && products.reduce((acc, cv) => [...acc, cv.category], []);
+  distinct_category = [...new Set(distinct_category)];
+
+  
+
   return (
     <nav className="w-[15%] h-full bg-zinc-200 flex flex-col items-center pt-5">
         <a
@@ -9,30 +19,20 @@ export default function Nav() {
         >
           Add new Product
         </a>
-        {/* <hr className='w-[70%]'/> */}
+        
         <h1 className=" mb-3 w-[80%] font-semibold">Category Filter</h1>
-        <ul className=" w-[80%]">
-          <li className=" mb-2 flex items-center ">
-            {" "}
-            <span className="mr-2 rounded-full w-[15px] h-[15px] bg-blue-300"></span>{" "}
-            cat 1
-          </li>
-          <li className=" mb-2 flex items-center ">
-            {" "}
-            <span className="mr-2 rounded-full w-[15px] h-[15px] bg-red-300"></span>{" "}
-            cat 2
-          </li>
-          <li className=" mb-2 flex items-center ">
-            {" "}
-            <span className="mr-2 rounded-full w-[15px] h-[15px] bg-green-300"></span>{" "}
-            cat 3
-          </li>
-          <li className=" mb-2 flex items-center ">
-            {" "}
-            <span className="mr-2 rounded-full w-[15px] h-[15px] bg-yellow-300"></span>{" "}
-            cat 4
-          </li>
-        </ul>
+      <div className=" w-[80%]">
+        {distinct_category.map((c, i) => (
+           <Link key={i} to ={`/?category=${c}`} className=" mb-2 flex items-center ">
+           {" "}
+           <span className="mr-2 rounded-full w-[15px] h-[15px] bg-blue-300"></span>{" "}
+           {c}
+         </Link>
+         ))}
+
+          
+          
+        </div>
       </nav>
   )
 }
